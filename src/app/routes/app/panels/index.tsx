1a0eb4a013fdb47573e666b14df3b80f25f30a4e
david.herve <david.herve.code@gmail.com>
2025-06-08 12:14:19 +0200

    feat: remove tanstack query utils

--- src/app/routes/app/panels/index.tsx
+++ src/app/routes/app/panels/index.tsx
@@ -1,13 +1,13 @@
 import { useParams } from 'react-router';
 
-import { Dashboard } from '@/features/central-panel';
+import { CentralPanel } from '@/features/central-panel';
 
 const Panels = () => {
   const { id } = useParams();
 
   return (
     <div className="h-screen bg-background text-foreground flex flex-col">
-      <Dashboard defaultPanels={[{ id: id! }]} />
+      <CentralPanel defaultPanels={[{ id: id! }]} />
     </div>
   );
 };
